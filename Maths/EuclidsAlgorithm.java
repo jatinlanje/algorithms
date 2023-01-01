cb544a9d337663e5be3086aaa941fc1d14ae66c5
Jatin Lanje <69909998+jatinlanje@users.noreply.github.com>
2023-01-01 09:32:37 +0530

    Update EuclidsAlgorithm.java

--- Maths/EuclidsAlgorithm.java
+++ Maths/EuclidsAlgorithm.java
@@ -2,7 +2,7 @@ public class EuclidsAlgorithm {
     public static void main(String[] args) {
         System.out.println(gcd(10,5));
     }
-    //Euclid's Algorithm can bve used to find the greatest common factor
+    //Euclid's Algorithm can be used to find the greatest common factor
     static int gcd(int A, int B) {
         A = Math.abs(A);
         B = Math.abs(B);
