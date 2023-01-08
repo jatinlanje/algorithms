2822409681b48f020f948976173e9d7edb1a02e9
Jatin Lanje <69909998+jatinlanje@users.noreply.github.com>
2023-01-08 21:31:32 +0530

    Create CheckSortedLinkedList.java

---
+++ Linked List/Singly Linked List/CheckSortedLinkedList.java
@@ -0,0 +1,50 @@
+public class CheckSortedLinkedList {
+    public static void main(String[] args) {
+        System.out.println();
+        ListNode node = new ListNode();
+        node.val=3;
+        node.next= new ListNode(6);
+        node.next.next= new ListNode(9);
+        node.next.next.next= new ListNode(12);
+        node.next.next.next.next= new ListNode(15);
+
+        node.printLinkedList(node); //3->6->9->12->15->
+        System.out.println();
+
+        System.out.println(node.checkSortedLinkedList(node)); //true
+    }
+}
+
+// nested class to define linked list node
+class ListNode{ //self-referential class
+    int val; //stores the element
+    ListNode next; //stores the reference to the next node in the list by default null
+
+    ListNode(){ //default constructor
+
+    }
+    ListNode(int val){ //parameterised constructor
+        this.val = val;
+        this.next=null; //optional in java but standard practice is to write this.next=null;
+    }
+
+    public void printLinkedList(ListNode head){
+        ListNode current=head;
+        while(current!=null){
+            System.out.print(current.val+"->");
+            current=current.next;
+        }
+        //System.out.println("NULL");
+    }
+
+    public boolean checkSortedLinkedList(ListNode head){ //ascending
+        ListNode current=head;
+        while(current.next!=null){
+            if(current.val>current.next.val){ //< for descending
+                return false;
+            }
+            current=current.next;
+        }
+        return true;
+    }
+}
