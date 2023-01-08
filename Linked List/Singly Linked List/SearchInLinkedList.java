1f9d5512b3bdb9c4ede9d43d087377be30961f68
Jatin Lanje <69909998+jatinlanje@users.noreply.github.com>
2023-01-08 20:59:24 +0530

    Create SearchInLinkedList.java

---
+++ Linked List/Singly Linked List/SearchInLinkedList.java
@@ -0,0 +1,48 @@
+public class SearchInLinkedList {
+    public static void main(String[] args) {
+        System.out.println();
+        ListNode node = new ListNode();
+        node.val=3;
+        node.next= new ListNode(6);
+        node.next.next= new ListNode(9);
+
+        node.printLinkedList(node);
+        System.out.println();
+        System.out.println(node.searchInLinkedList(node,1));
+
+    }
+}
+
+// nested class to define linked list node
+class ListNode{ //self-referential class
+    int val; //stores the element
+    ListNode next; //stores the reference to the next node in the list by default null
+
+    ListNode(){ //default constructor
+
+    }
+    ListNode(int val){ //parameterised constructor
+        this.val = val;
+        this.next=null; //optional in java but standard practice is to write this.next=null;
+    }
+
+    public void printLinkedList(ListNode head){
+        ListNode current=head;
+        while(current!=null){
+            System.out.print(current.val+"->");
+            current=current.next;
+        }
+        //System.out.println("NULL");
+    }
+
+    public boolean searchInLinkedList(ListNode head, int k){
+        ListNode current=head;
+        while(current!=null){
+            if(current.val==k){
+                return true;
+            }
+            current=current.next;
+        }
+        return false;
+    }
+}
