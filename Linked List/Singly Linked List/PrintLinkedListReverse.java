3c4da924a1e7ad668f0239f7f37237d826e9b506
Jatin Lanje <69909998+jatinlanje@users.noreply.github.com>
2023-01-09 22:35:04 +0530

    Create PrintLinkedListReverse.java

---
+++ Linked List/Singly Linked List/PrintLinkedListReverse.java
@@ -0,0 +1,50 @@
+public class PrintLinkedListReverse{
+    public static void main(String[] args) {
+        System.out.println();
+        ListNode node = new ListNode();
+        node.val=3;
+        node.next= new ListNode(6);
+        node.next.next= new ListNode(9);
+        node.next.next.next= new ListNode(12);
+        node.next.next.next.next= new ListNode(15);
+
+        node.printLinkedList(node); //3->6->9->12->15->
+        System.out.println();
+
+        node.printLinkedListReverse(node); //15->12->9->6->3
+
+    }
+}
+
+// nested class to define linked list node
+class ListNode{ //self-referential class
+    int val; //stores the element
+    ListNode next; //stores the reference to the next node in the list by default null
+    ListNode tail;
+
+    ListNode(){ //default constructor
+
+    }
+    ListNode(int val){ //parameterised constructor
+        this.val = val;
+        this.next=null; //optional in java but standard practice is to write this.next=null;
+    }
+
+    public void printLinkedList(ListNode head){
+        ListNode current=head;
+        while(current!=null){
+            System.out.print(current.val+"->");
+            current=current.next;
+        }
+        //System.out.println("NULL");
+    }
+
+    public void printLinkedListReverse(ListNode head){
+        if(head==null){
+            return;
+        }
+        printLinkedListReverse(head.next);
+        System.out.print(head.val+"->");
+    }
+
+}
