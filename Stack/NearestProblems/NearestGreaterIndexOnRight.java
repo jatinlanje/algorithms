c8911ac30186ac03e428802f3ce8a174666e1fcb
Jatin Lanje <69909998+jatinlanje@users.noreply.github.com>
2023-01-16 21:45:08 +0530

    Create NearestGreaterIndexOnRight.java

---
+++ Stack/NearestProblems/NearestGreaterIndexOnRight.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+public class NearestGreaterIndexOnRight {
+    public static void main(String[] args) {
+        int arr[]=new int[]{4, 5, 2, 10, 8};
+        System.out.println(Arrays.toString(nearestSmallerIndexOnRight(arr)));
+    }
+    public static int[] nearestSmallerIndexOnRight(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[arr.length];
+
+        // Iterate through the array in reverse order
+        for (int i = arr.length-1; i >= 0; i--) {
+            // While stack is not empty and the top element of the stack is less or equal to the current element
+            while (!stack.isEmpty() && arr[stack.peek()] <= arr[i]) {
+                stack.pop();
+            }
+            // If the stack is empty, set the result to -1
+            if (stack.isEmpty()) {
+                result[i] = -1;
+            } 
+            // Else set the result to the index of the nearest greater element
+            else {
+                result[i] = stack.peek();
+            }
+            // Push the current element index onto the stack
+            stack.push(i);
+        }
+        return result;
+    }
+}
