a734e1d28b73ed4267008d7010fc38e1fa5840d0
Jatin Lanje <69909998+jatinlanje@users.noreply.github.com>
2023-04-16 18:23:33 +0530

    Update MergeTwoSortedLinkedList.java

--- Linked List/Singly Linked List/MergeTwoSortedLinkedList.java
+++ Linked List/Singly Linked List/MergeTwoSortedLinkedList.java
@@ -70,4 +70,24 @@ class ListNode{ //self-referential class
             return list2;
         }
     }
+    
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while(l1!=null && l2!=null){
+            //Merge List
+            if(l1.val <= l2.val) {
+                curr.next = l1;
+                l1=l1.next;
+            }
+            else{
+                curr.next = l2;
+                l2=l2.next;
+            }
+            curr= curr.next;
+        }
+        curr.next= l1!=null ? l1 : l2;
+        return dummy.next;
+    }
+
 }
