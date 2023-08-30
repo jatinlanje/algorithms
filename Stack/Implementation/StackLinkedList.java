b277e25b89ead82e2a8125c02e0d50f38a1e7d56
Jatin Lanje <69909998+jatinlanje@users.noreply.github.com>
2023-08-30 19:11:10 +0530

    Update StackLinkedList.java

--- Stack/Implementation/StackLinkedList.java
+++ Stack/Implementation/StackLinkedList.java
@@ -1,28 +1,10 @@
 public class StackLinkedList {
-    public static void main(String[] args) {
-        StackLinkedList myStack = new StackLinkedList(); // creates a stack
-        myStack.push(1); // pushes the value 1 onto the stack
-        myStack.push(2); // pushes the value 2 onto the stack
-        myStack.push(3); // pushes the value 3 onto the stack
 
-        myStack.display(); // displays the contents of the stack: 3 2 1
-
-//        while(!myStack.isEmpty()){ // display
-//            System.out.print(myStack.peek()+" ");
-//            myStack.pop();
-//        }
-
-        int poppedValue = myStack.pop(); // removes and returns the value at the top of the stack (3)
-        System.out.println("Popped value: " + poppedValue); // prints "Popped value: 3"
-
-        myStack.display(); // displays the contents of the stack: 2 1
-    }
-
-    static class Node {
+    static class Node{
         int data;
         Node next;
 
-        public Node(int data) {
+        public Node(int data){
             this.data=data;
             next=null;
         }
@@ -30,52 +12,47 @@ public class StackLinkedList {
 
     public static Node head;
 
-    // check if stack is empty
-    public static boolean isEmpty() {
-        return head == null;
+    public static boolean isEmpty(){
+        return head==null;
     }
 
-    // push value into stack
-    public static void push(int value) {
-        Node newNode = new Node(value);
+    public static void push(int val){
+        Node newNode = new Node(val);
         if(isEmpty()){
             head=newNode;
             return;
         }
-        newNode.next = head;
-        head = newNode;
+        newNode.next=head;
+        head=newNode;
     }
 
-    // pop value from stack
-    public static int pop() {
-        if (isEmpty()) {
-            System.out.println("Stack is empty!");
+    public static int pop(){
+        if(isEmpty()){
+            System.out.println("Stack is empty");
             return -1;
-        } else {
-            int top = head.data;
-            head = head.next;
-            return top;
         }
+        int val= head.data;
+        head = head.next;
+        return val;
     }
 
     public static int peek(){
-        if (isEmpty()) {
-            System.out.println("Stack is empty!");
+        if (isEmpty()){
+            System.out.println("Stack is Empty");
             return -1;
-        } else {
-            return head.data;
         }
+        return head.data;
     }
 
-    // display stack elements
-    public void display() {
-        if (isEmpty()) {
-            System.out.println("Stack is empty!");
-        } else {
-            Node current = head;
-            while (current != null) {
+    public static void display(){
+        if(isEmpty()){
+            System.out.println("Stack is Empty");
+        }
+        else{
+            Node current=head;
+            while(current!=null){
                 System.out.print(current.data + " ");
-                current = current.next;
+                current=current.next;
             }
             System.out.println();
         }
