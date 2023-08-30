ec2c35de02079b3f27a45b90434a19fb3f05a78e
Jatin Lanje <69909998+jatinlanje@users.noreply.github.com>
2023-08-30 19:10:34 +0530

    Update StackArrayList.java

--- Stack/Implementation/StackArrayList.java
+++ Stack/Implementation/StackArrayList.java
@@ -1,65 +1,37 @@
-import java.util.ArrayList;
+public class StackArrayList {
+    List<Integer> stack = new ArrayList<>();
 
-public class StackArrayList<T> {
-    public static void main(String[] args) {
-        StackArrayList<Integer> myStack = new StackArrayList<Integer>();
-        myStack.push(1); // pushes the value 1 onto the stack
-        myStack.push(2); // pushes the value 2 onto the stack
-        myStack.push(3); // pushes the value 3 onto the stack
-
-        myStack.display(); // displays the contents of the stack: 3 2 1
-
-        // Pop an element off the stack
-        int poppedValue = myStack.pop(); // removes and returns the value at the top of the stack (3)
-        System.out.println("Popped value: " + poppedValue); // prints "Popped value: 3"
-
-        myStack.display(); // displays the contents of the stack: 2 1
-
+    public boolean isEmpty(){
+        return stack.isEmpty();
     }
 
-    ArrayList<Integer> list = new ArrayList<>(); // underlying data structure
-
-    // check if stack is empty
-    public boolean isEmpty() {
-        return list.size() == 0;
+    public void push(int val){
+        stack.add(val);
     }
 
-    // push method
-    public void push(int data) {
-        // add the item at the end
-        list.add(data);
-    }
-
-    // pop method
-    public int pop() {
-        if (isEmpty()) {
-            System.out.println("Stack is empty");
+    public int pop(){
+        if(isEmpty()){
+            System.out.println("Stack is Empty");
             return -1;
         }
-        // remove the item at the end
-        int top=list.get(list.size()-1);
-        list.remove(list.size()-1);
-        return top;
+        return stack.remove(stack.size()-1);
     }
 
-    // peek method
     public int peek(){
-        if (isEmpty()) {
-            System.out.println("Stack is empty");
+        if(isEmpty()){
+            System.out.println("Stack is Empty");
             return -1;
         }
-        return list.get(list.size()-1);
+        return stack.get(stack.size()-1);
     }
 
-    public void display() {
-        if (isEmpty()) {
-            System.out.println("Stack is empty!");
-        } else {
-            for (int i = list.size()-1; i >= 0; i--) {
-                System.out.print(list.get(i) + " ");
-            }
-            System.out.println();
+    public void display(){
+        if(isEmpty()){
+            System.out.println("Stack is Empty");
         }
+        for(int i=stack.size()-1; i>=0; i--){
+            System.out.print(stack.get(i) + " ");
+        }
+        System.out.println();
     }
 }
-
